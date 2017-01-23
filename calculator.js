52e0140794377e43033a343ce70c886d2de9f6b0
Alex Luecke <alex.james.luecke@gmail.com>
2017-01-23 02:32:03 +0000

    comment styles and replace bad input

--- calculator.js
+++ calculator.js
@@ -11,38 +11,44 @@ function hasOp(arg) {
     return arg && isNaN(parseInt(arg)) && Object.keys(operators).some(op => arg.indexOf(op) !== -1);
 }
 
-// Example operator definition:
-//
-// function plus(arg) {
-//    return d + [(typeof arg === 'function') ? arg() : arg, '+'].join(d);
-// }
-//
-// Generate all operator definitions:
+/**
+* Example operator definition:
+*
+* function plus(arg) {
+*    return d + [(typeof arg === 'function') ? arg() : arg, '+'].join(d);
+* }
+*
+* Generate all operator definitions:
+* */
 Object.keys(operators).forEach(op => {
     window[operators[op].name] = (arg) => d + [(typeof arg === 'function') ? arg() : arg, op].join(d);
 });
 
-// Example number definition:
-//
-// function one(arg) {
-//    return (typeof arg === 'string') ? calculate(1 + arg) : 1;
-// }
-//
-// Generate all number definitions:
+/**
+* Example number definition:
+*
+* function one(arg) {
+*    return (typeof arg === 'string') ? calculate(1 + arg) : 1;
+* }
+*
+* Generate all number definitions:
+* */
 ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven'].forEach(
     (name, value) => {
         window[name] = (arg) => hasOp(arg) ? rpnCalculator(value + arg /* order matters! */) : value;
     }
 );
 
-// Reverse polish notation calculator since we defined our string representation in RPN
+/**
+* Reverse polish notation calculator
+* */
 function rpnCalculator(str) {
-    let stack = [];
-    let acc = 0;
+    var stack = [];
+    var acc = 0;
     str.replace(/\s/g, '')
         .split(d) // numbers and operators are delimited to allow any number (like 11) instead of 0-9
         .forEach(x => {
-            let v = parseInt(x);
+            var v = parseInt(x);
             if (isNaN(v)) {
                 // all operators take 2 operands, if one is not defined the equation is malformed
                 // order of arguments must match the verbage one(dividedBy(two)) = '12/',
@@ -61,7 +67,7 @@ function run(inputId, outputId) {
     const input = document.querySelector(inputId);
     const output = document.querySelector(outputId);
     try {
-        output.innerHTML=eval(input.value);
+        output.innerHTML=eval(input.value.replace(/[^A-Za-z0-9\(\)]/));
     } catch(e) {
         console.log(e);
         output.innerHTML='Input failed';
